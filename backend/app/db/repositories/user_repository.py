13b7a6567871fe1806ed8074e8da6b56eadd7467
cleanmind777 <cleanmind77777@gmail.com>
2025-06-17 04:14:02 +0900

    create: db/repositries/user_repository.py

---
+++ backend/app/db/repositories/user_repository.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from app.models.user import User
+from app.schemas.user import UserCreate
+from app.core.security import hash_password
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(User).filter(User.username == username).first()
+
+def create_user(db: Session, user_create: UserCreate):
+    db_user = User(
+        username=user_create.username,
+        email=user_create.email,
+        full_name=user_create.full_name,
+        hashed_password=hash_password(user_create.password),
+        disabled=False,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user

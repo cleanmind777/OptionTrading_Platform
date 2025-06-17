fa8db0526ffb2438bf0b588e2e36b6730680d6e3
cleanmind777 <cleanmind77777@gmail.com>
2025-06-17 16:28:16 +0900

    fix: email->username

--- backend/app/api/v1/endpoints/auth.py
+++ backend/app/api/v1/endpoints/auth.py
@@ -17,7 +17,7 @@ def sign_up(user_create: UserCreate, db: Session = Depends(get_db)):
 
 @router.post("/token", response_model=Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.email, form_data.password)
+    user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email or password",
                             headers={"WWW-Authenticate": "Bearer"})
